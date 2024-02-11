9fe8f76c8a7b3a456ad50980e0cd1d4d38c935ab
safak <drapius@hotmail.com>
2024-02-11 20:51:45 +0300

    refactor: performance optimized.

--- app/Console/Commands/ParseXML.php
+++ app/Console/Commands/ParseXML.php
@@ -2,10 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Product;
 use App\Services\Product\ProductCreator;
 use App\Services\Xml\XmlParser;
 use Illuminate\Console\Command;
 use GuzzleHttp\Client;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class ParseXML extends Command
@@ -45,10 +49,9 @@ class ParseXML extends Command
      */
 
 
-    public function __construct(ProductCreator $productCreator, XmlParser $xmlService)
+    public function __construct(XmlParser $xmlService)
     {
         parent::__construct();
-        $this->productCreator = $productCreator;
         $this->xmlService = $xmlService;
     }
 
@@ -62,20 +65,41 @@ class ParseXML extends Command
         $this->info('XmlParser executing...');
         Log::info('XmlParser executing...');
         try {
+            DB::beginTransaction();
             $client = new Client();
             $response = $client->get(config('xml.url'));
 
             $contents = $response->getBody()->getContents();
             $xml = $this->xmlService->parse($contents);
+            $products = [];
 
             foreach ($xml->product as $productXml) {
-                $this->productCreator->createOrUpdateProductFromXml($productXml);
+                $products[] = [
+                    'id' => (int) $productXml->id,
+                    'name' => (string) $productXml->name,
+                    'description' => (string) $productXml->description,
+                    'price' => (float) $productXml->price,
+                    'quantity' => (int) $productXml->quantity,
+                    'photo_url' => (string) $productXml->photo_url,
+                ];
             }
+            $chunked = array_chunk($products, 100);
+            array_map(function ($chunk) {
+                Product::upsert(
+                    $chunk,
+                    ['id'],
+                    ['name', 'description', 'price', 'quantity', 'photo_url']
+                );
+            }, $chunked);
+
+            DB::commit();
             Log::info('XmlParser executed successfully.');
             $this->info('XmlParser executed successfully.');
         } catch (\Exception $e) {
+            DB::rollback();
             Log::error('An error occurred during execution of XmlParser: ' . $e->getMessage());
             $this->error('An error occurred during execution of XmlParser.');
         }
+        return 0;
     }
 }
